cb705333fc27395b2bf2751d945167bebbcfd74e
Keven Qiu <qiukeven@icloud.com>
2023-03-17 13:29:09 -0600

    Modified regression

--- LinearRegression.R
+++ LinearRegression.R
@@ -1,11 +1,11 @@
-firstfn <- "/Volumes/Seagate Backup Plus Drive/WBCT Reconstruction/segmentation images/coronal_wbct/profile.csv"
-secondfn <- "/Volumes/Seagate Backup Plus Drive/WBCT Reconstruction/segmentation images/PKOA_112_wbct/profile.csv"
+firstfn <- "/Volumes/Seagate Backup Plus Drive/WBCT Reconstruction/segmentation images/PKOA_112_clinical/profile.csv"
+secondfn <- "/Volumes/Seagate Backup Plus Drive/WBCT Reconstruction/segmentation images/PKOA_112_wbct/profileResampled.csv"
 
 firstData <- read.csv(firstfn)
 secondData <- read.csv(secondfn)
 
-firstBmd <- firstData$BMD800
-secondBmd <- secondData$BMD800
+firstBmd <- firstData$BMD100
+secondBmd <- secondData$BMD100
 
 firstLength <- length(firstBmd)
 secondLength <- length(secondBmd)
@@ -18,10 +18,11 @@ if (firstLength <= secondLength) {
 
 model <- lm(secondBmd~firstBmd)
 
-title <- expression("BMD 800 mgHA/cm"^3)
-xLabel <- "Mean (cCT.Ph and cCT.Pa)"
-yLabel <- "Error (cCT.Ph - cCT.Pa)"
+title <- expression("BMD 100 mgHA/cm"^3)
+xLabel <- "cCT.Pa"
+yLabel <- "WBCT.Pa"
 
-legend()
-
-plot(firstBmd, secondBmd, main = title, xlab = xLabel, ylab = yLabel)
+plot(secondBmd~firstBmd, main = title, xlab = xLabel, ylab = yLabel, pch=19)
+abline(model, col='red', lwd=2)
+legend("topleft", 4, legend=c(paste("y =", format(model$coefficients[1],digits=7), "+", format(model$coefficients[2],digits=5),"x"), as.expression(bquote(r^2 * " = " * .(summary(model)$r.squared)))), bty='n')
+summary(model)
