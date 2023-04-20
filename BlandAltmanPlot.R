c3a3c5dea99de389f4e7081c2aa8f8af9c480594
Keven Qiu <qiukeven@icloud.com>
2023-04-20 16:14:38 -0600

    Changes to BA and LG

--- BlandAltmanPlot.R
+++ BlandAltmanPlot.R
@@ -1,5 +1,5 @@
-firstfn <- "/Volumes/Seagate Backup Plus Drive/WBCT Reconstruction/segmentation images/PKOA_112_wbct/profileResampled.csv"
-secondfn <- "/Volumes/Seagate Backup Plus Drive/WBCT Reconstruction/segmentation images/PKOA_112_clinical/profile.csv"
+firstfn <- "~/Documents/wbctbmddata/segmentation images/coronal_wbct/profileResampled.csv"
+secondfn <- "~/Documents/wbctbmddata/segmentation images/coronal_clinical/profile.csv"
 
 firstData <- read.csv(firstfn)
 secondData <- read.csv(secondfn)
@@ -9,12 +9,10 @@ secondBmd <- secondData$BMD100
 
 firstLength <- length(firstBmd)
 secondLength <- length(secondBmd)
+minLength <- min(firstLength, secondLength)
 
-if (firstLength <= secondLength) {
-  secondBmd <- secondBmd[1:firstLength]
-} else {
-  firstBmd <- firstBmd[1:secondLength]
-}
+firstBmd <- firstBmd[1:minLength]
+secondBmd <- secondBmd[1:minLength]
 
 df <- data.frame(bmd1 = firstBmd, bmd2 = secondBmd)
 
@@ -27,6 +25,11 @@ print(meanDiff)
 
 model <- lm(difference~mean, data = df)
 
+tStar <- qt(0.975, df = minLength - 1)
+
+lowerCI <- meanDiff - tStar * sd(df$difference)
+upperCI <- meanDiff + tStar * sd(df$difference)
+
 newX <- seq(min(df$mean), max(df$mean), length.out=length(df$difference))
 prediction <- predict(model, newdata = data.frame(mean=newX), interval='confidence')
 
@@ -38,6 +41,8 @@ plot(difference~mean, data = df, main = title, xlab = xLabel, ylab = yLabel, pch
 text(x=max(df$mean), y=meanDiff, bquote(bar(x) * " = " * .(format(meanDiff, digits=4))))
 abline(model, col='red', lwd=2)
 abline(h=0,col='grey',lty=1, lwd=1) 
-lines(newX, prediction[,3], col='blue', lty = 2, lwd = 2)
-lines(newX, prediction[,2], col='blue', lty = 2, lwd = 2)
+abline(h=lowerCI, col='blue', lty = 2, lwd = 2)
+abline(h=upperCI, col='blue', lty = 2, lwd = 2)
+# lines(newX, prediction[,3], col='blue', lty = 2, lwd = 2)
+# lines(newX, prediction[,2], col='blue', lty = 2, lwd = 2)
 summary(model)
--- LinearRegression.R
+++ LinearRegression.R
@@ -1,28 +1,43 @@
-firstfn <- "/Volumes/Seagate Backup Plus Drive/WBCT Reconstruction/segmentation images/coronal_wbct/profileResampled.csv"
-secondfn <- "/Volumes/Seagate Backup Plus Drive/WBCT Reconstruction/segmentation images/coronal_clinical/profile.csv"
+firstfn <- "~/Documents/wbctbmddata/segmentation images/coronal_wbct/profileResampled.csv"
+secondfn <- "~/Documents/wbctbmddata/segmentation images/coronal_clinical/profile.csv"
 
 firstData <- read.csv(firstfn)
 secondData <- read.csv(secondfn)
 
-firstBmd <- firstData$BMD800
-secondBmd <- secondData$BMD800
+firstBmd100 <- firstData$BMD100
+secondBmd100 <- secondData$BMD100
+firstBmd400 <- firstData$BMD400
+secondBmd400 <- secondData$BMD400
+firstBmd800 <- firstData$BMD800
+secondBmd800 <- secondData$BMD800
 
-firstLength <- length(firstBmd)
-secondLength <- length(secondBmd)
+firstLength <- length(firstBmd800)
+secondLength <- length(secondBmd800)
 
-if (firstLength <= secondLength) {
-  secondBmd <- secondBmd[1:firstLength]
-} else {
-  firstBmd <- firstBmd[1:secondLength]
-}
+minLength <- min(firstLength, secondLength)
+
+firstBmd100 <- firstBmd100[1:minLength]
+secondBmd100 <- secondBmd100[1:minLength]
+firstBmd400 <- firstBmd400[1:minLength]
+secondBmd400 <- secondBmd400[1:minLength]
+firstBmd800 <- firstBmd800[1:minLength]
+secondBmd800 <- secondBmd800[1:minLength]
+
+firstBmd <- c(firstBmd100, firstBmd400, firstBmd800)
+secondBmd <- c(secondBmd100, secondBmd400, secondBmd800)
 
 model <- lm(secondBmd~firstBmd)
 
-title <- expression("BMD 800 mgHA/cm"^3)
+newX <- seq(min(firstBmd), max(firstBmd), length.out=length(firstBmd))
+prediction <- predict(model, newdata = data.frame(mean=newX), interval='confidence')
+
 xLabel <- "WBCT.Ph"
 yLabel <- "cCT.Ph"
+title <- paste(xLabel, "vs.", yLabel) # expression("BMD 800 mgHA/cm"^3)
 
 plot(secondBmd~firstBmd, main = title, xlab = xLabel, ylab = yLabel, pch=19)
 abline(model, col='red', lwd=2)
+lines(newX, prediction[,3], col='blue', lty = 2, lwd = 2)
+lines(newX, prediction[,2], col='blue', lty = 2, lwd = 2)
 legend("topleft", 4, legend=c(paste("y =", format(model$coefficients[1],digits=7), "+", format(model$coefficients[2],digits=5),"x"), as.expression(bquote(r^2 * " = " * .(summary(model)$r.squared)))), bty='n')
 summary(model)
